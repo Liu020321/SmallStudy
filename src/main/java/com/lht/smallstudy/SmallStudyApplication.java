dc9079d6ce8dcb35e2fef02ae23a44fbc1feec86
Liu Haitao <76166358+Liu020321@users.noreply.github.com>
2023-09-04 09:23:09 +0800

    First Push

---
+++ src/main/java/com/lht/smallstudy/SmallStudyApplication.java
@@ -0,0 +1,13 @@
+package com.lht.smallstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallStudyApplication.class, args);
+	}
+
+}
